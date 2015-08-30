16217dc30299c87e90cba3fc2389d6ff9f2a449d
Chandan Kumar Bandaru <bchandan2@gmail.com>
2015-08-30 17:02:45 +0530

    added query parameter to listVoicemessages

--- MyDoctorGAE/src/com/sspharma/api/VoiceMessageApi.java
+++ MyDoctorGAE/src/com/sspharma/api/VoiceMessageApi.java
@@ -4,45 +4,83 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiClass;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
 import com.google.api.server.spi.response.CollectionResponse;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.ParentReference;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.LoadType;
+import com.googlecode.objectify.cmd.Query;
 import com.sspharma.bean.Doctor;
 import com.sspharma.bean.User;
 import com.sspharma.bean.VoiceMessage;
-import com.sspharma.constants.EnvConstants;
 import com.sspharma.util.DriveUtil;
 
 @Api(name = "api", version = "v1", namespace = @ApiNamespace(ownerDomain = "sspharma.com", ownerName = "sspharma.com", packagePath = "") )
 @ApiClass(resource = "voicemessages")
 public class VoiceMessageApi {
 
-	HttpTransport trasport = new UrlFetchTransport();
-	JsonFactory jsonFactory = new JacksonFactory();
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyy");
 
-	@ApiMethod(name = "voicemessages.listDoctors")
-	public CollectionResponse<VoiceMessage> listDoctors() {
-		List<VoiceMessage> voiceMessages = ofy().load().type(VoiceMessage.class).list();
+	/**
+	 * 
+	 * @param q Query parameter.<br>
+	 * 			Examples : 1. doctor=abc@gmail.com<br>
+	 * 				       2. user=abc@gmail.com<br>
+	 * 					   3. doctor=abc@gmail.com,user=cba@gmail.com<br>
+	 * @return list of <code>VoiceMessage</code>s
+	 */
+	@ApiMethod(name = "voicemessages.listVoiceMessages")
+	public CollectionResponse<VoiceMessage> listVoiceMessages(@Named("q") @Nullable String q) {
+		System.out.println(q);
+		
+		List<VoiceMessage> voiceMessages = null;
+		if(q == null)
+		{
+			voiceMessages = ofy().load().type(VoiceMessage.class).list();
+		}
+		else
+		{
+			String[] conditions = q.split(",");
+			LoadType<VoiceMessage> loadType = ofy().load().type(VoiceMessage.class);
+			Query<VoiceMessage> query=null;
+			for(String condition : conditions)
+			{
+				String[] conditionKeyValue = condition.split("=");
+				if(conditionKeyValue[0].equalsIgnoreCase("doctor"))
+				{
+					if(query == null)
+						query = loadType.filter("doctorEmail", conditionKeyValue[1]);
+					else
+						query = query.filter("doctorEmail", conditionKeyValue[1]);
+				}
+				else if(conditionKeyValue[0].equalsIgnoreCase("user"))
+				{
+					
+					if(query == null)
+						query = loadType.filter("patientEmail", conditionKeyValue[1]);
+					else
+						query = query.filter("patientEmail", conditionKeyValue[1]);
+				}
+			}
+			if(query != null)
+			{
+				voiceMessages = query.list();
+			}
+			else
+			{
+				voiceMessages = loadType.list();
+			}
+			
+		}
 		return new CollectionResponse.Builder<VoiceMessage>().setItems(voiceMessages).build();
 	}
-
 	@ApiMethod(name = "voicemessages.insertVoiceMessage")
 	public VoiceMessage insertVoiceMessage(VoiceMessage voiceMessage) throws IOException {
 		Doctor doctor = ofy().load().type(Doctor.class).id(voiceMessage.getDoctorEmail()).now();
