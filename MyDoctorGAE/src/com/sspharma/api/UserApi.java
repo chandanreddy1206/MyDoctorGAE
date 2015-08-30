b6dd1fdd71cd009891932c3e3db0dc82a418d53a
Chandan Kumar Bandaru <bchandan2@gmail.com>
2015-08-30 17:02:10 +0530

    removed unnecessary imports

--- MyDoctorGAE/src/com/sspharma/api/UserApi.java
+++ MyDoctorGAE/src/com/sspharma/api/UserApi.java
@@ -5,10 +5,6 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 import java.io.IOException;
 import java.util.List;
 
-import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiClass;
 import com.google.api.server.spi.config.ApiMethod;
@@ -24,9 +20,6 @@ import com.sspharma.util.DriveUtil;
 @ApiClass(resource = "users")
 public class UserApi {
 	
-	HttpTransport trasport = new UrlFetchTransport();
-	JsonFactory jsonFactory = new JacksonFactory();
-
 	@ApiMethod(name = "users.listUsers")
 	public CollectionResponse<User> listUsers() {
 		List<User> users = ofy().load().type(User.class).list();
