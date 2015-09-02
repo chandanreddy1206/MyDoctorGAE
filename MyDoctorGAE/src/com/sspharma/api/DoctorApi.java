703622a5136e12eccc099ea9863bdb094f554670
Chandan Kumar Bandaru <bchandan2@gmail.com>
2015-09-02 23:33:14 +0530

    added query parameter to listDoctors and added listCategories() api method

--- MyDoctorGAE/src/com/sspharma/api/DoctorApi.java
+++ MyDoctorGAE/src/com/sspharma/api/DoctorApi.java
@@ -3,16 +3,21 @@ package com.sspharma.api;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiClass;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
 import com.google.api.server.spi.response.CollectionResponse;
 import com.google.api.server.spi.response.ConflictException;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.LoadType;
+import com.googlecode.objectify.cmd.Query;
 import com.sspharma.bean.Doctor;
 import com.sspharma.constants.EnvConstants;
 import com.sspharma.util.DriveUtil;
@@ -20,11 +25,47 @@ import com.sspharma.util.DriveUtil;
 @Api(name = "api", version = "v1", namespace = @ApiNamespace(ownerDomain = "sspharma.com", ownerName = "sspharma.com", packagePath = "") )
 @ApiClass(resource = "doctors")
 public class DoctorApi {
-	
+	/**
+	 * 
+	 * @param q Query parameter.<br>
+	 * 			Examples : 1. category=gp<br>
+	 * 				       2. category=gynecologist<br>
+	 * @return list of <code>VoiceMessage</code>s
+	 */
 	@ApiMethod(name = "doctors.listDoctors")
-	public CollectionResponse<Doctor> listDoctors() {
+	public CollectionResponse<Doctor> listDoctors(@Named("q") @Nullable String q) {
 		
-		List<Doctor> doctors = ofy().load().type(Doctor.class).list();
+		List<Doctor> doctors = null;
+		if(q == null)
+		{
+			doctors = ofy().load().type(Doctor.class).list();
+		}
+		else
+		{
+			String[] conditions = q.split(",");
+			LoadType<Doctor> loadType = ofy().load().type(Doctor.class);
+			Query<Doctor> query=null;
+			for(String condition : conditions)
+			{
+				String[] conditionKeyValue = condition.split("=");
+				if(conditionKeyValue[0].equalsIgnoreCase("category"))
+				{
+					if(query == null)
+						query = loadType.filter("category", conditionKeyValue[1]);
+					else
+						query = query.filter("category", conditionKeyValue[1]);
+				}
+			}
+			if(query != null)
+			{
+				doctors = query.list();
+			}
+			else
+			{
+				doctors = loadType.list();
+			}
+			
+		}
 		return new CollectionResponse.Builder<Doctor>().setItems(doctors).build();
 	}
 
@@ -52,5 +93,17 @@ public class DoctorApi {
 	public Doctor updateDoctor(Doctor doctor) {
 		Key<Doctor> key = ofy().save().entity(doctor).now();
 		return ofy().load().key(key).now();
+	}
+	@ApiMethod(name = "doctors.categories.listCategories",path="doctors/categories")
+	public CollectionResponse<String> getAllCategories()
+	{
+		List<String> categories = new ArrayList<>();
+		List<Doctor> doctors = ofy().load().type(Doctor.class).project("category").distinct(true).list();
+		System.out.println(doctors);
+		for(Doctor doctor : doctors)
+		{
+			categories.add(doctor.getCategory());
+		}
+		return new CollectionResponse.Builder<String>().setItems(categories).build();
 	}
 }
